988eeb584c164b0ff1d004d82ea41da4421b675f
Mahmoud Salah <mahmoud.salah@pixelogicmedia.com>
2019-04-21 16:45:41 +0200

    new build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./lib/App";
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
--- src/lib/App.js
+++ src/lib/App.js
@@ -4,9 +4,10 @@ import './App.css';
 
 import {
   Route,
-  Link,
+  NavLink,
   Switch,
-  Redirect
+  Redirect,
+  BrowserRouter
 } from 'react-router-dom';
 
 import Home from './components/Home';
@@ -16,6 +17,7 @@ import Messages from './components/Messages';
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -23,9 +25,9 @@ class App extends Component {
         </header>
         <div className="menu">
           <ul>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/messages">Messages</Link> </li>
-            <li> <Link to="/about">About</Link> </li>
+            <li> <NavLink to="/">Home</NavLink> </li>
+            <li> <NavLink to="/messages">Messages</NavLink> </li>
+            <li> <NavLink to="/about">About</NavLink> </li>
           </ul>
         </div>
         <div className="App-intro">
@@ -37,6 +39,7 @@ class App extends Component {
           </Switch>
         </div>
       </div>
+      </BrowserRouter>
     );
   }
 }
